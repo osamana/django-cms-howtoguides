7d132838de056014bde75326b751d6c36505a6d8
osama abuomar <osamana@outlook.com>
2016-12-11 01:49:28 +0200

    fix settings.py

--- mydjango/myproject/myproject/settings.py
+++ mydjango/myproject/myproject/settings.py
@@ -172,3 +172,10 @@ LANGUAGES = [
 ]
 
 
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    # 'easy_thumbnails.processors.scale_and_crop',  # disable this one
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters'
+)
